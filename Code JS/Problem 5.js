84f3a98c775ead401a1da46076e32a53f331f632
NAvrosjo <navrosjo@gmail.com>
2025-05-08 21:54:39 +0700

    Update Problem 5.js

--- Code JS/Problem 5.js
+++ Code JS/Problem 5.js
@@ -3,6 +3,3 @@ function isPalindrome(str){
     const reversed = str.split("").reverse().join("");
     return str === reversed;
 }
-
-console.log(isPalindrome("madam"));
-console.log(isPalindrome("hello"));
